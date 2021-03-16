9b4e2694f87bdfb93f5e54116af96e347ca1de2c
samsisle <samko9522@gmail.com>
2021-03-16 13:06:38 -0700

    Update Next.js

--- hub/next.config.js
+++ hub/next.config.js
@@ -1,16 +1,11 @@
-const TWENTYTWENTY_URL = "https://vercel-test-2020.vercel.app";
 const TWENTYTWENTYONE_URL = "https://vercel-test-2021.vercel-support.app";
 
 module.exports = {
   async rewrites() {
     return [
       {
-        source: "/2020",
-        destination: `${TWENTYTWENTY_URL}/2020`,
-      },
-      {
         source: "/2021",
-        destination: `${TWENTYTWENTYONE_URL}`,
+        destination: `${TWENTYTWENTYONE_URL}`
       },
     ];
   },
