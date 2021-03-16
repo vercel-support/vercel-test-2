6261b4b24fa76c3846d0214bc8e8dc6a10b13f72
samsisle <samko9522@gmail.com>
2021-03-16 12:20:09 -0700

    Remove basePath, update rewrites

--- hub/next.config.js
+++ hub/next.config.js
@@ -1,16 +1,16 @@
-const TWENTYTWENTY_URL = 'https://vercel-test-2020.vercel.app';
-const TWENTYTWENTYONE_URL = 'https://vercel-test-2021.vercel.app';
+const TWENTYTWENTY_URL = "https://vercel-test-2020.vercel.app";
+const TWENTYTWENTYONE_URL = "https://vercel-test-2.vercel-support.app";
 
 module.exports = {
-  rewrites() {
+  async rewrites() {
     return [
       {
-        source: '/2020',
+        source: "/2020",
         destination: `${TWENTYTWENTY_URL}/2020`,
       },
       {
-        source: '/2021',
-        destination: `${TWENTYTWENTYONE_URL}/2021`,
+        source: "/2021",
+        destination: `${TWENTYTWENTYONE_URL}`,
       },
     ];
   },
